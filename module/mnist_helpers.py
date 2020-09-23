c985dba28b9dcdaf13d063edadb591314075ed20
Will Sutton <wsutton17@gmail.com>
2020-09-23 16:09:51 -0400

    lots of old work; basecamp

--- module/mnist_helpers.py
+++ module/mnist_helpers.py
@@ -18,6 +18,15 @@ from matplotlib import pyplot as plt
 from fastai2.basics import *    
 from fastai2.vision.all import *
 
+def seed_everything(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
 def point_from_img(fn, path):
 
     img_np = np.array(Image.open(str(path) + fn))
